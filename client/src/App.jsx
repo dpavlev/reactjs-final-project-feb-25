fbe0d31d6feb1b7b2ad75a570f3c21ddcd4caac9
dpavlev <dimitar.pavlev03@gmail.com>
2025-03-26 20:37:54 +0200

    Add page addStudio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
 import { Routes, Route } from "react-router";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Register from "./views/Register";
+import AddStudio from "./views/AddStudio";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/addStudio" element={<AddStudio />} />
             </Routes>
             <Footer />
         </>
---
+++ client/src/views/AddStudio.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import FormButton from "../components/common/FormButton";
+import formsStyles from "./Forms.module.css";
+
+export default function AddStudio() {
+    const [values, setValues] = useState({
+        studioName: "",
+        studioAddress: "",
+        studioPhone: "",
+        studioDescription: "",
+        studioImg: "",
+        qtyServices: 0,
+        services: []
+    });
+
+    const updateServices = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleQtyChange = (e) => {
+        const qty = Number(e.target.value);
+        setValues((state) => ({
+            ...state,
+            qtyServices: qty,
+            services: Array(qty)
+                .fill()
+                .map(() => ({ name: "", price: 0 }))
+        }));
+    };
+
+    const handleServiceUpdate = (index, field, value) => {
+        setValues((state) => ({
+            ...state,
+            services: state.services.map((service, i) => (i === index ? { ...service, [field]: value } : service))
+        }));
+    };
+
+    const submitAction = () => {
+        // TODO: Send the data to the server
+        console.log(values);
+    };
+
+    return (
+        <main className={formsStyles.main}>
+            <div className={formsStyles.loginContainer}>
+                <form action={submitAction} className={`${formsStyles.formContent} ${formsStyles.loginForm}`}>
+                    <input type="text" name="studioName" id="studioName" placeholder="Име на студиото..." className={formsStyles.formDiv} value={values.studioName} onChange={updateServices} />
+                    <input type="text" name="studioAddress" id="studioAddress" placeholder="Адрес..." className={formsStyles.formDiv} value={values.studioAddress} onChange={updateServices} />
+                    <input type="tel" name="studioPhone" id="studioPhone" placeholder="Телефон: +359 8..." className={formsStyles.formDiv} value={values.studioPhone} onChange={updateServices} />
+                    <textarea name="studioDescription" id="description" placeholder="Описание..." className={`${formsStyles.formDiv} ${formsStyles.formTextarea}`} value={values.studioDescription} onChange={updateServices}></textarea>
+                    <input type="url" name="studioImg" id="studioImg" placeholder="Image URL..." className={formsStyles.formDiv} value={values.studioImg} onChange={updateServices} />
+                    <input type="number" name="qtyServices" id="qtyServices" min="1" className={formsStyles.formDiv} value={values.qtyServices} onChange={handleQtyChange} />
+                    <div className={formsStyles.servicesContainer}>
+                        {Array.from({ length: values.qtyServices }, (_, index) => (
+                            <div key={index} className={formsStyles.servicesInput}>
+                                <input type="text" className={formsStyles.formDiv} placeholder={`Service ${index + 1} name...`} value={values.services[index]?.name || ""} onChange={(e) => handleServiceUpdate(index, "name", e.target.value)} />
+                                <input type="number" className={formsStyles.formDiv} placeholder="Price..." min="0" step="0.01" value={values.services[index]?.price || 0} onChange={(e) => handleServiceUpdate(index, "price", e.target.value)} />
+                            </div>
+                        ))}
+                    </div>
+                    <FormButton text="Add Studio" />
+                </form>
+            </div>
+        </main>
+    );
+}

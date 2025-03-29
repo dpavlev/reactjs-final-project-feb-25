e934a9a88e9c40e0dc5c095894c2f19ac0f34f1d
dpavlev <dimitar.pavlev03@gmail.com>
2025-03-30 00:30:04 +0200

    Configure express server and database connection

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import routes from "./routes.js";
+
+const app = express();
+
+try {
+    const databaseUrl = "mongodb://localhost:27017/react-project";
+    await mongoose.connect(databaseUrl);
+    console.log("Database Connected Successfully!");
+} catch (err) {
+    console.log("Cannot connect to DB!");
+    console.error(err.message);
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.listen(5000, () => {
+    console.log("Server is listening on http://localhost:5000...");
+});

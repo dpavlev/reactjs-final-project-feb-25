2eada70bc05d2ee710689366cf2d38423a5bf254
dpavlev <dimitar.pavlev03@gmail.com>
2025-03-31 14:46:20 +0300

    Add dynamic navigation

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,28 +1,40 @@
 import { Link } from "react-router";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Header() {
+    const { id, isStudio } = useContext(UserContext);
     return (
         <header className="header">
             <Link to="/" className="header-logo-background">
                 <img src="/images/logo_rectangle.png" alt="StyleGo" className="header-logo-size" />
             </Link>
             <nav className="nav hide-on-small">
-                {/* <div className="searchbar-container">
-                    <i className="fa-solid fa-magnifying-glass magglass"></i>
-                    <input className="searchbar" type="search" name="search" placeholder="Search" />
-                </div> */}
-                <Link to="/login" className="button">
-                    Login
-                </Link>
-                <Link to="/register" className="button">
-                    Register
-                </Link>
-                <Link to="/userProfile" className="button">
-                    Profile
-                </Link>
-                <Link to="/logout" className="button">
-                    Logout
-                </Link>
+                {id ? (
+                    <>
+                        {isStudio ? (
+                            <Link to="/studioView" className="button">
+                                Studio Profile
+                            </Link>
+                        ) : (
+                            <Link to="/userProfile" className="button">
+                                Profile
+                            </Link>
+                        )}
+                        <Link to="/logout" className="button">
+                            Logout
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login" className="button">
+                            Login
+                        </Link>
+                        <Link to="/register" className="button">
+                            Register
+                        </Link>
+                    </>
+                )}
             </nav>
         </header>
     );
